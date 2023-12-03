441595c44ad5a2d5a7566001e1e974c943e4272f
mps3058 <152104226+mps3058@users.noreply.github.com>
2023-12-04 09:11:27 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -138,3 +138,4 @@ let Firstfleet_ship = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+controller.moveSprite(Firstfleet_ship, 100, 100)
